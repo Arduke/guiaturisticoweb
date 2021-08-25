31bdb57253587afd34922b6adeb5960fbeecc40d
Carlos Eduardo Martins Filho <carlos.filho@fcamara.com.br>
2021-08-25 12:32:31 -0300

    feat(dev): added configuration for PWA interactions

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { registerServiceWorker } from "./serviceWorker";
+registerServiceWorker();
 ReactDOM.render(
   <React.StrictMode>
     <App />
